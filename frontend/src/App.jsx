fa64ab7b318842f732c3ee6181361cb54c35b037
techwithsatish <satish15.dev@gmail.com>
2025-04-29 21:01:54 +0530

    created desktop UI  and  now will add responsive design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,13 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 {/* rafc*/}
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
@@ -23,7 +24,7 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
-        <Route path='/login'element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/place-order' element={<PlaceOrder />} />
         <Route path='/orders' element={<Orders />} />
       </Routes>
