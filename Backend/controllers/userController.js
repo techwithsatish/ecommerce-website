05ff28e88fd4b6f8ee4f6c9b4aa4824494ebdf55
techwithsatish <satish15.dev@gmail.com>
2026-04-30 22:15:06 +0530

    Add user authentication routes and controller functions

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+
+
+// Route for user login
+const loginUser = async (req, res) => {}
+
+
+// Route for user register
+const registerUser = async (req, res) => {
+    res.json({msg:"Register API Working"})
+}
+
+// Route for admin login
+const adminLogin = async (req, res) => {}
+
+
+export { loginUser, registerUser, adminLogin }
--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js';
-
+import userRouter from './routes/userRoute.js'
 
 
 
@@ -20,6 +20,9 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+// api endpoints
+app.use('/api/user',userRouter)
 
 
 
