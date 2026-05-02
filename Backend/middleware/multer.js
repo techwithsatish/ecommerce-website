dec28bea5b427ba01f27a0ad8b134e7b56526434
techwithsatish <satish15.dev@gmail.com>
2026-05-02 12:33:52 +0530

    Added image upload functionality to product addition

---
+++ Backend/middleware/multer.js
@@ -0,0 +1,14 @@
+// I did not Understand the This middleware Logic.
+
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    filename:function(req,file,callback){
+        callback(null,file.originalname)
+    }
+})
+
+const upload = multer({storage})
+
+export default upload
